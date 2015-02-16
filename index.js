6459d8517b3981f38c1fb2fa51310396ea0c5482
kbierbaum <kate@girldevelopit.com>
2015-02-15 19:25:25 -0700

    adding dummy index file for jade

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var scarf = require('./models/scarf');
 var weather = require('./models/weather');
+var jade = require('jade');
 
 var app = module.exports = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.set('view engine', 'jade');
 
 app.get('/scarf', function(req, res) {
 	var zip = '80303';
@@ -28,6 +30,10 @@ app.get('/scarf', function(req, res) {
 	})
 	
 });
+
+app.get('/', function (req, res) {
+	res.render('index', { title: 'Weather Scarf', message: 'Weather scarves are awesome!'});
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
